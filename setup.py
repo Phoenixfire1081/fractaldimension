73a45ec2b28b5c7b5ff6cb5b5a2e32f59b570e63
Abhishek <abi.abhishek7@gmail.com>
2023-11-28 11:52:14 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
   
 setuptools.setup( 
     name="boxcounting", 
-    version="1.0.0.0", 
+    version="1.0.0.1", 
     author="Abhishek Harikrishnan", 
     author_email="abhishek.harikrishnan@fu-berlin.de", 
     packages=["boxcounting"], 
